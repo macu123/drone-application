2f515ea7cc9217e5f62ecf9b4e38fca1c37f6b28
John Chen <chenjiapeng12345@gmail.com>
2021-06-20 13:25:45 -0400

    create engine class

---
+++ engine.rb
@@ -0,0 +1,27 @@
+class Engine
+  attr_reader :power
+
+  STATUSES = %i[off on].freeze
+
+  def initialize
+    @power = 0
+    @status = :off
+  end
+
+  STATUSES.each do |status|
+    define_method "turn_#{status}" do
+      @status = status
+    end
+
+    define_method "is_#{status}?" do
+      @status == status
+    end
+  end
+
+  def set_power(power)
+    return false unless (power >= 0) && (power <= 100)
+
+    @power = power
+    true
+  end
+end
